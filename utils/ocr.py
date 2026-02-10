10e57a85cd528d87d4699da029e773915e5944b0
Dembele <faballademble82@gmail.com>
2026-02-10 03:26:30 +0000

    update2

--- utils/ocr.py
+++ utils/ocr.py
@@ -6,19 +6,16 @@ reader = easyocr.Reader(['en', 'fr'])
 
 def ocr_multi(img):
     texts = []
-    results = reader.readtext(img)
-    if results:
-          #merged_text = merge_easyocr_blocks(results)
-#         merged_text = clean_text(merged_text)
-          fixed = enforce_plate_rules(results)
-          if fixed:
-               texts.append(fixed)
+    for r in reader.readtext(img):
+        texts.append(r[1])
     tess = pytesseract.image_to_string(
-        img,
-        config="--psm 6 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-"
+    img,
+    config="--psm 5 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-"
     )
     texts.append(tess)
     return texts
+
+
 
 
 def clean_and_validate(texts):
@@ -31,72 +28,3 @@ def clean_and_validate(texts):
     return max(set(candidates), key=candidates.count) if candidates else "NON_DETECTE"
 
 
-# Corrections OCR courantes
-LETTER_FIX = {
-    '0': 'O',
-    '1': 'I',
-    '5': 'S',
-    '8': 'B',
-    'U': 'M',
-    'D': 'O' # cas fréquent sur plaques
-}
-
-DIGIT_FIX = {
-    'O': '0',
-    'D': '0',
-    'I': '1',
-    'S': '5',
-    'B': '8'
-}
-
-
-# def clean_text(text):
-#     text = text.upper()
-#     text = re.sub(r'[^A-Z0-9-]', '', text)
-#     return text
-
-
-def enforce_plate_rules(text):
-
-    if len(text) < 4:
-        return None
-
-    chars = list(text)
-
-    # 2 premières lettres
-    for i in [0, 1]:
-        if chars[i].isdigit():
-            chars[i] = LETTER_FIX.get(chars[i], chars[i])
-
-    # Dernière lettre
-    if chars[-1].isdigit():
-        chars[-1] = LETTER_FIX.get(chars[-1], chars[-1])
-
-    return ''.join(chars)
-
-
-def merge_easyocr_blocks(results, x_threshold=50):
-    """
-    Fusionne les blocs OCR éloignés horizontalement
-    """
-    blocks = []
-
-    for bbox, text, conf in results:
-        x_coords = [p[0] for p in bbox]
-        x_center = int(np.mean(x_coords))
-        blocks.append((x_center, text))
-
-    # Trier de gauche à droite
-    blocks = sorted(blocks, key=lambda x: x[0])
-
-    merged = blocks[0][1]
-    last_x = blocks[0][0]
-
-    for x, text in blocks[1:]:
-        if abs(x - last_x) < x_threshold:
-            merged += text
-        else:
-            merged += "-" + text  # séparation logique
-        last_x = x
-
-    return merged
